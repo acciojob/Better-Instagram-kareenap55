0a1af9b4526d0dfe8f59607a2a75f85cd82e7c87
kareenap55 <kareenap1412@gmail.com>
2025-04-04 09:41:00 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+const draggables = document.querySelectorAll('.draggable');
+let draggedElement = null;
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', function (e) {
+        draggedElement = this;
+        e.dataTransfer.setData('text/plain', this.id);
+    });
+
+    draggable.addEventListener('dragover', function (e) {
+        e.preventDefault();
+    });
+
+    draggable.addEventListener('drop', function (e) {
+        e.preventDefault();
+        if (draggedElement !== this) {
+            let tempBg = this.style.backgroundImage;
+            this.style.backgroundImage = draggedElement.style.backgroundImage;
+            draggedElement.style.backgroundImage = tempBg;
+        }
+    });
+});
+
